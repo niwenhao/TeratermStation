8b09c8462b84064319b4c1d6348bc71e3a250de6
ISC塩崎 <isc00254@partner.nri.co.jp>
2016-03-25 13:17:42 +0900

    License.

--- src/main/java/jp/co/tabocom/teratermstation/preference/AboutPage.java
+++ src/main/java/jp/co/tabocom/teratermstation/preference/AboutPage.java
@@ -1,5 +1,7 @@
 package jp.co.tabocom.teratermstation.preference;
 
+import jp.co.tabocom.teratermstation.Main;
+
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
@@ -10,8 +12,6 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Label;
 
-import jp.co.tabocom.teratermstation.Main;
-
 public class AboutPage extends PreferencePage {
 
     public AboutPage() {
@@ -20,28 +20,60 @@ public class AboutPage extends PreferencePage {
 
     @Override
     protected Control createContents(Composite parent) {
-        final Composite composite = new Composite(parent, SWT.NONE);
-        composite.setLayout(new GridLayout(3, false));
-        Label icon = new Label(composite, SWT.NONE);
+
+        GridLayout parentGrLt = new GridLayout(1, false);
+        parentGrLt.verticalSpacing = 20;
+        parent.setLayout(parentGrLt);
+
+        Composite appGrp = new Composite(parent, SWT.NONE);
+        appGrp.setLayout(new GridLayout(3, false));
+        GridData appGrpGrDt = new GridData(GridData.FILL_HORIZONTAL);
+        appGrp.setLayoutData(appGrpGrDt);
+
+        Label icon = new Label(appGrp, SWT.NONE);
         GridData iconGrDt = new GridData();
         iconGrDt.verticalSpan = 3;
         iconGrDt.widthHint = 70;
         icon.setLayoutData(iconGrDt);
         Image iconImg = new Image(parent.getDisplay(), Main.class.getClassLoader().getResourceAsStream("icon48.png"));
         icon.setImage(iconImg);
-        
-        Label versionTitleLbl = new Label(composite, SWT.NONE);
+
+        Label versionTitleLbl = new Label(appGrp, SWT.NONE);
         GridData versionTitleLblGrDt = new GridData();
         versionTitleLblGrDt.widthHint = 100;
         versionTitleLbl.setLayoutData(versionTitleLblGrDt);
         versionTitleLbl.setText("Version:");
-        Label versionValueLbl = new Label(composite, SWT.NONE);
+        Label versionValueLbl = new Label(appGrp, SWT.NONE);
         GridData versionValueLblGrDt = new GridData();
         versionValueLbl.setLayoutData(versionValueLblGrDt);
         versionValueLbl.setText("1.1.1");
-        
+
+        Label copyrightLbl = new Label(appGrp, SWT.NONE);
+        GridData copyrightLblGrDt = new GridData();
+        copyrightLblGrDt.horizontalSpan = 2;
+        copyrightLbl.setLayoutData(copyrightLblGrDt);
+        copyrightLbl.setText("Copyright (C) 2015 - 2016 Tabocom All Rights Reserved.");
+
+        Label urlLbl = new Label(appGrp, SWT.NONE);
+        GridData urlLblGrDt = new GridData();
+        urlLblGrDt.horizontalSpan = 2;
+        urlLbl.setLayoutData(urlLblGrDt);
+        urlLbl.setText("https://github.com/turbou/TeratermStation");
+
+        Composite licenseGrp = new Composite(parent, SWT.NONE);
+        GridLayout licenseGrpGrLt = new GridLayout(1, false);
+        licenseGrp.setLayout(licenseGrpGrLt);
+        GridData licenseGroupGrDt = new GridData(GridData.FILL_BOTH);
+        licenseGrp.setLayoutData(licenseGroupGrDt);
+
+        Label licenseLbl = new Label(licenseGrp, SWT.NONE);
+        licenseLbl.setText("This software includes the work that is distributed in the Apache License 2.0");
+
+        Label snakeYamlLbl = new Label(licenseGrp, SWT.NONE);
+        snakeYamlLbl.setText("- SnakeYAML");
+
         noDefaultAndApplyButton();
-        return composite;
+        return parent;
     }
 
     @Override
