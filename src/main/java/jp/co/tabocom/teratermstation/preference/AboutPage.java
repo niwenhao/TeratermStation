5ad5933472b023db77d37eaf806140054003c3fa
turbou <turbou@i.softbank.jp>
2017-12-07 14:01:43 +0900

    version up.

--- src/main/java/jp/co/tabocom/teratermstation/preference/AboutPage.java
+++ src/main/java/jp/co/tabocom/teratermstation/preference/AboutPage.java
@@ -46,7 +46,7 @@ public class AboutPage extends PreferencePage {
         Label versionValueLbl = new Label(appGrp, SWT.NONE);
         GridData versionValueLblGrDt = new GridData();
         versionValueLbl.setLayoutData(versionValueLblGrDt);
-        versionValueLbl.setText("2.0.0");
+        versionValueLbl.setText("2.1.0");
 
         Label copyrightLbl = new Label(appGrp, SWT.NONE);
         GridData copyrightLblGrDt = new GridData();
